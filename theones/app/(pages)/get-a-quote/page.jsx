ce3e6316b574ee31d17e80759d65e946b89f5dea
Kelvin Kimutai <kiruikevinkim@gmail.com>
2025-04-03 20:35:12 +0300

    updated header and faqs

--- theones/app/(pages)/get-a-quote/page.jsx
+++ theones/app/(pages)/get-a-quote/page.jsx
@@ -28,7 +28,7 @@ export default function GetAQuote() {
   };
 
   return (
-    <div className="w-full max-w-[1280px] mx-auto px-6 py-20 mt-20 mb-20 h-[65vh]">
+    <div className="w-full max-w-[1280px] mx-auto px-6 py-20 mt-20 mb-20 min-h-[85vh] h-full">
 <div className="max-w-4xl mx-auto p-6 ">
       {/* Progress Bar */}
       <div className="relative w-full bg-gray-300 h-2 rounded-full">
