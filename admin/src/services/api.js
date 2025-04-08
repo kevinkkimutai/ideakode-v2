f554957499c3b8642dd5ede346a404d0d662fad0
Kelvin Kimutai <kiruikevinkim@gmail.com>
2025-04-08 23:03:15 +0300

    fixed token againss again

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -2,8 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:2200/api/",
-  // baseUrl:"https://ideakode-v2-1.onrender.com/api/",
+  // baseUrl: "http://localhost:2200/api/",
+  baseUrl:"https://ideakode-v2-1.onrender.com/api/",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
