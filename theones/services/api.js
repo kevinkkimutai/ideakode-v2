bcf61e925904f5d0b2d2ed1585882518a6f49237
Kelvin Kimutai <kiruikevinkim@gmail.com>
2025-04-09 00:07:16 +0300

    fixed token againss again cloud

--- theones/services/api.js
+++ theones/services/api.js
@@ -4,7 +4,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
   // baseUrl: "http://localhost:2200/api/",
-  baseUrl:"https://ideakode-v2-1.onrender.com/api/",
+  // baseUrl:"https://ideakode-v2-1.onrender.com/api/",
+  baseUrl: "https://api.netiqa.co.ke/api/",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const state = getState();
